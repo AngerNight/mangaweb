5c61997cc559fe63b07396e9e4fd37e897f69d87
Dogukan Togan <dogukantgn444@gmail.com>
2023-02-07 01:32:28 +0300

    07.01.2023

--- public/js/live_search.js
+++ public/js/live_search.js
@@ -10,7 +10,7 @@ function search() {
       xhr.open("GET", `/api/D3H6G1J7K2M8/${searchValue}`);
       xhr.onload = function() {
         if (xhr.status === 200) {
-          searchResults.style.display = "block";
+          searchResults.style.display = "flex";
           var data = JSON.parse(xhr.responseText);   
           searchResults.innerHTML = "";
           if (data.length === 0) { 
@@ -19,9 +19,20 @@ function search() {
           } else {
             noItems.style.display = "none";
             data.forEach(function(item) {
-            var li = document.createElement("li");    
-              li.textContent = item.manga_title;
-              searchResults.appendChild(li);
+              var div = document.createElement("div");
+              var a = document.createElement("a");
+              var img = document.createElement("img");
+              var span = document.createElement("span");
+              a.classList.add("item-3");
+              img.classList.add("item-img");
+              span.classList.add("item-title");
+              a.href = ("/comics/"+item.manga_url)
+              img.src = item.manga_image
+              span.textContent = item.manga_title
+              searchResults.appendChild(div)
+              div.appendChild(a)
+              a.appendChild(img)
+              a.appendChild(span)
             });
           }
         }
@@ -29,3 +40,19 @@ function search() {
       xhr.send();
     }
   }
+
+  // var li = document.createElement("li");
+  // var a = document.createElement("a");
+  // var img = document.createElement("img");
+  // var span = document.createElement("span");
+  // a.classList.add("item-3");
+  // img.classList.add("item-img");
+  // span.classList.add("item-title");
+  // a.href = item.manga_url
+  // img.src = item.manga_image
+  // span.textContent = item.manga_title
+  // searchResults.appendChild(li)
+  //   li.appendChild(a)
+  //   a.appendChild(img)
+  //   a.appendChild(span)
+ 
